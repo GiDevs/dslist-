94cc7c3ea7d6ab40323861b1f8a3c1e6979a5118
Geovanna <geovannadiasnery@gmail.com>
2025-01-23 00:27:46 -0300

    GET game by id

--- src/main/java/com/geovannad/dslist/services/GameService.java
+++ src/main/java/com/geovannad/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.geovannad.dslist.dto.GameDTO;
 import com.geovannad.dslist.dto.GameMinDTO;
 import com.geovannad.dslist.entities.Game;
 import com.geovannad.dslist.repositories.GameRepository;
@@ -14,7 +16,15 @@ public class GameService {
 	
 		@Autowired
 		private GameRepository gameRepository;
+		
+		@Transactional(readOnly = true)
+		public GameDTO findById(Long id) {
+			Game result = gameRepository.findById(id).get();
+			return new GameDTO(result);
+			
+		}
 
+		@Transactional(readOnly = true)
 		public List<GameMinDTO> findAll() {
 				List<Game> result = gameRepository.findAll();
 				return result.stream(). map(x -> new GameMinDTO(x)).toList();		
